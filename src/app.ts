357b3a6d61e6b338cd4c7120dab0d6f295ddb51a
Rebeca Baruch <71520936+RebecaBaruch@users.noreply.github.com>
2022-12-22 11:14:27 -0300

    fix: start page

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,4 @@
-console.log("Hello World!");
+const backButton: HTMLButtonElement = document.getElementById('backBtn')! as HTMLButtonElement;
+const startBtn:HTMLButtonElement = document.getElementById('formStart')! as HTMLButtonElement;
+
+backButton.addEventListener('click', window.history.back());
