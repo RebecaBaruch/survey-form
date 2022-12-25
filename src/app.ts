e86c614e9106e52a5f0a7259e794bee548d3be0d
Rebeca Baruch <71520936+RebecaBaruch@users.noreply.github.com>
2022-12-25 13:43:43 -0300

    fix: I had to get the success.ts back

--- src/app.ts
+++ src/app.ts
@@ -101,34 +101,3 @@ if(skipBtn){
         nextURL();
     });
 }
-
-//Success page
-class userOutput {
-    userOutput: HTMLElement;
-    emailOutput: HTMLElement;
-    ageOutput: HTMLElement;
-
-    inputName: string;
-    inputEmail: string;
-    inputAge: string;
-
-    constructor() {
-        this.userOutput = document.getElementById('userOutput')! as any;
-        this.emailOutput = document.getElementById('emailOutput')! as HTMLElement;
-        this.ageOutput = document.getElementById('ageOutput')! as HTMLElement;
-
-        this.inputName = localStorage.getItem('user')!;
-        this.inputEmail = localStorage.getItem('email')!;
-        this.inputAge = localStorage.getItem('age')!;
-
-        this.configure()
-    }
-
-    private configure() {
-        this.userOutput.innerText = this.inputName;
-        this.emailOutput.innerText = this.inputEmail;
-        this.ageOutput.innerText = this.inputAge;
-    }
-}
-
-const userOutputElement = new userOutput();
