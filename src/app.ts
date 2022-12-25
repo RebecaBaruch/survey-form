29af435688d616832d78fc5c99477edf74f1ef7d
Rebeca Baruch <71520936+RebecaBaruch@users.noreply.github.com>
2022-12-25 13:02:33 -0300

    fix: back navigation

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
+//The getElementsByClassName method returns an array-like object (not an array).
 const radioOp: HTMLElement[] = document.querySelectorAll('.radioOp')! as any;
 let submitBtn: HTMLInputElement = document.querySelector('.sbmAccount')! as HTMLInputElement;
-//The getElementsByClassName method returns an array-like object (not an array).
 const checkbox: HTMLCollection[] = document.querySelectorAll('.checkBox')! as any;
 
 let radioValue = ' ';
@@ -8,14 +8,14 @@ let selected: number;
 selected = 0;
 
 //function for button abled to submit the form
-function sbmAbled() {
+const sbmAbled = () => {
     submitBtn.classList.remove('disabledBtn');
     submitBtn.classList.add('abled');
     submitBtn.disabled = false;
 }
 
 //function for button disabled to submit the form
-function sbmDisabled() {
+const sbmDisabled = () => {
     submitBtn.classList.remove('abled');
     submitBtn.classList.add('disabledBtn');
     submitBtn.disabled = true;
@@ -39,29 +39,61 @@ const radioChecked = radioOp.forEach((input: any) => {
 //checkbox validation
 //issues to fix: its not able to select the same checkbox after back history
 const boxChecked = checkbox.forEach((input: any) => {
-    input.addEventListener('click', () => {
-        radioValue = input.value;
-        console.log(radioValue);
-        console.log(input.checked)
-        
-        if(input.checked) {
-            selected++;
-            if(selected >= 1) sbmAbled();
-        }else {
-            selected--;
-            if(selected === 0) sbmDisabled();      
+    let storageSel: string;
+    if(!input.checked) {
+        input.addEventListener('click', () => {
+            
+            if(input.checked) {
+                selected++;
+                if(selected >= 1) {
+                    sbmAbled();
+
+                    storageSel = selected.toString();
+                    localStorage.setItem('selected', storageSel);
+    
+                    console.log(storageSel);
+                }
+
+            }
+            if(!input.checked) {
+                selected--;
+                if(selected == 0){
+                    sbmDisabled(); 
+
+                    storageSel = selected.toString();
+                    localStorage.setItem('selected', storageSel);
+
+                    console.log(storageSel)
+                }      
+            }
+        });
+    }
+});
+
+const backBtn: HTMLElement = document.querySelector('.backBtn') as any;
+
+const navigation: any = [
+    '/public/index.html',
+    '/src/pages/Start/start.html',
+    '/src/pages/form01/form01.html',
+    '/src/pages/form02/form02.html',
+    '/src/pages/form03/form03.html',
+    '/src/pages/form04/form04.html',
+    'src/pages/success/success.html'
+];
+
+if(backBtn){
+    backBtn.addEventListener('click', () => {
+        const atualURL = () => {
+            navigation.forEach((item: any, index: any) => {
+                if(window.location.pathname === item){
+                    window.location.pathname = navigation[index-1];
+                }
+            });
         }
-    })
-});
-
-const backBtn: HTMLElement[] = document.querySelectorAll('.backBtn')! as any;
-
-const backHistory = backBtn.forEach((input: any) => {
-    input.addEventListener('click', () => {
-        window.history.back();
-        localStorage.clear();
-    })
-});
+        atualURL();
+    });
+}
 
 //--------------------------------------------------------------------------------------
 //user data validation
